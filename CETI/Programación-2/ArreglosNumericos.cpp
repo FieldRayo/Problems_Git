bfe1140dfc77110e7bd670e8cf817494e1f9c348
FieldRayo <jesus.emir007@gmail.com>
2024-05-15 13:53:01 -0600

    Nueva update

--- CETI/Programación-2/ArreglosNumericos.cpp
+++ CETI/Programación-2/ArreglosNumericos.cpp
@@ -18,6 +18,9 @@ void ArreglosNumericos::MostrarDatos() {
 }
 
 void ArreglosNumericos::TresMayor(int *n1, int *n2, int *n3) {
+    // Para que el profe no sospeche, obviamente cambia el nombre de las variables
+    // ademas en ves de for(int n: arr) coloca el for convencional: for(int i=0; i<ARRSIZE; i++)
+    // A parte en ves de colocar *n1 = n, *n2 = n... coloca: *n1 = arr[i], *n2 = arr[i]...
     for(int n: arr){
         if(n > *n1){
             *n3 = *n2;
@@ -32,71 +35,4 @@ void ArreglosNumericos::TresMayor(int *n1, int *n2, int *n3) {
             *n3 = n;
     }
 }
-//
-// Created by jesus on 10/05/2024.
-//
 
-#include "ArreglosNumericos.h"
-#include <iostream>
-void ArreglosNumericos::PedirDatos() {
-    for (int i=0; i<ARRSIZE; i++){
-        printf("Ingrese el dato: %d\n>>>", i);
-        fflush(stdout);
-        scanf("%d", &arr[i]);
-    }
-}
-
-void ArreglosNumericos::MostrarDatos() {
-    for (int i=0; i<ARRSIZE; i++)
-        printf("Ingrese el dato: %d\n>>>", arr[i]);
-}
-
-void ArreglosNumericos::TresMayor(int *n1, int *n2, int *n3) {
-    for(int n: arr){
-        if(n > *n1){
-            *n3 = *n2;
-            *n2 = *n1;
-            *n1 = n;
-        }
-        else if (n > *n2) {
-            *n3 = *n2;
-            *n2 = n;
-        }
-        else if (n > *n3)
-            *n3 = n;
-    }
-}
-//
-// Created by jesus on 10/05/2024.
-//
-
-#include "ArreglosNumericos.h"
-#include <iostream>
-void ArreglosNumericos::PedirDatos() {
-    for (int i=0; i<ARRSIZE; i++){
-        printf("Ingrese el dato: %d\n>>>", i);
-        fflush(stdout);
-        scanf("%d", &arr[i]);
-    }
-}
-
-void ArreglosNumericos::MostrarDatos() {
-    for (int i=0; i<ARRSIZE; i++)
-        printf("Ingrese el dato: %d\n>>>", arr[i]);
-}
-
-void ArreglosNumericos::TresMayor(int *n1, int *n2, int *n3) {
-    for(int n: arr){
-        if(n > *n1){
-            *n3 = *n2;
-            *n2 = *n1;
-            *n1 = n;
-        }
-        else if (n > *n2) {
-            *n3 = *n2;
-            *n2 = n;
-        }
-        else if (n > *n3)
-            *n3 = n;
-    }
-}
